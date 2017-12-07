c1f165a66f16f960ad07a2f9a6086e5a4b5cadf4
= <cuillgln@live.com>
2017-12-07 18:58:12 +0800

    add overlay layer

--- src/main/resources/static/mx/dataMine/layer.js
+++ src/main/resources/static/mx/dataMine/layer.js
@@ -7,6 +7,77 @@ LayerControl.prototype = new mxLib.Control();
 
 LayerControl.prototype.initialize = function(map) {
 	var div = document.createElement("div");
+	for (var i = 1; i <= 4; i++) {
+		// <p id="layerName" ><checkedbox/><span>layerName</span></p>
+		var layerDiv = document.createElement("p");
+		layerDiv.style.margin="2px";
+		layerDiv.setAttribute("id", "overlay_" + i);
+		var checkbox = document.createElement("input");
+		checkbox.setAttribute("type", "checkbox");
+		checkbox.setAttribute("checked", "checked");
+		layerDiv.appendChild(checkbox);
+		var span = document.createElement("span");
+		switch(i) {
+		case 1:
+			span.appendChild(document.createTextNode("传感器层"));
+			layerDiv.onclick = function(e) {
+				var targetDiv = e.currentTarget;
+				var checkbox = targetDiv.getElementsByTagName("input")[0];
+				if (checkbox.checked) {
+					setOverlayInvisible(1);
+					checkbox.checked = false;
+				} else {
+					setOverlayVisible(1);
+					checkbox.checked = true;
+				}
+			}
+			break;
+		case 2:
+			span.appendChild(document.createTextNode("安全监控分站"));
+			layerDiv.onclick = function(e) {
+				var targetDiv = e.currentTarget;
+				var checkbox = targetDiv.getElementsByTagName("input")[0];
+				if (checkbox.checked) {
+					setOverlayInvisible(2);
+					checkbox.checked = false;
+				} else {
+					setOverlayVisible(2);
+					checkbox.checked = true;
+				}
+			}
+			break;
+		case 3:
+			span.appendChild(document.createTextNode("人员定位分站"));
+			layerDiv.onclick = function(e) {
+				var targetDiv = e.currentTarget;
+				var checkbox = targetDiv.getElementsByTagName("input")[0];
+				if (checkbox.checked) {
+					setOverlayInvisible(3);
+					checkbox.checked = false;
+				} else {
+					setOverlayVisible(3);
+					checkbox.checked = true;
+				}
+			}
+			break;
+		case 4:
+			span.appendChild(document.createTextNode("语音广播分站"));
+			layerDiv.onclick = function(e) {
+				var targetDiv = e.currentTarget;
+				var checkbox = targetDiv.getElementsByTagName("input")[0];
+				if (checkbox.checked) {
+					setOverlayInvisible(4);
+					checkbox.checked = false;
+				} else {
+					setOverlayVisible(4);
+					checkbox.checked = true;
+				}
+			}
+			break;
+		}
+		layerDiv.appendChild(span);
+		div.appendChild(layerDiv);
+	}
 
 	var layers = eval(map.getLayer());
 	for (var i = 0; i < layers.length; i++) {
@@ -49,4 +120,61 @@ function setLayerVisible(map, layerName) {
 }
 function setLayerInvisible(map, layerName) {
 	map.setLayerAttribute(layerName, 'DWG_LAYER_VISBLE', 'false');
+}
+
+function setOverlayVisible(i) {
+	switch(i) {
+	case 1:
+		for ( var i in window.sensor.sensorMap) {
+			var marker = window.sensor.sensorMap[i];
+			marker.show();
+		}
+		break;
+	case 2:
+		for ( var i in window.sensor.smsMap) {
+			var marker = window.sensor.smsMap[i];
+			marker.show();
+		}
+		break;
+	case 3:
+		for ( var i in window.sensor.spsMap) {
+			var marker = window.sensor.spsMap[i];
+			marker.show();
+		}
+		break;
+	case 4:
+		for ( var i in window.sensor.absMap) {
+			var marker = window.sensor.absMap[i];
+			marker.show();
+		}
+		break;
+	}
+}
+function setOverlayInvisible(i) {
+	switch(i) {
+	case 1:
+		for ( var i in window.sensor.sensorMap) {
+			var marker = window.sensor.sensorMap[i];
+			marker.hide();
+		}
+		break;
+	case 2:
+		for ( var i in window.sensor.smsMap) {
+			var marker = window.sensor.smsMap[i];
+			marker.hide();
+		}
+		break;
+	case 3:
+		for ( var i in window.sensor.spsMap) {
+			var marker = window.sensor.spsMap[i];
+			marker.hide();
+		}
+		break;
+	case 4:
+		for ( var i in window.sensor.absMap) {
+			var marker = window.sensor.absMap[i];
+			marker.hide();
+		}
+		break;
+	}
 }
